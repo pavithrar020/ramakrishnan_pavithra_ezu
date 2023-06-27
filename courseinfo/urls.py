6ef1f7910acec0aaf70fb78a286bdeeaddc680ff
pavithrar020 <dr23@illinois.edu>
2023-06-27 13:11:26 -0700

    Initial commit

---
+++ courseinfo/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from courseinfo.views import (
+    instructor_list_view,
+    section_list_view,
+    course_list_view,
+    semester_list_view,
+    student_list_view,
+    registration_list_view,
+)
+
+urlpatterns = [
+    path('instructor/', instructor_list_view),
+    path('section/', section_list_view),
+    path('course/', course_list_view),
+    path('semester/', semester_list_view),
+    path('student/', student_list_view),
+    path('registration/', registration_list_view),
+]
